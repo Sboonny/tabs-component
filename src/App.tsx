9fd9616d1f51b48e3ea51e1bd0bbdf8c9f975b6c
Sboonny <muhammedelruby@gmail.com>
2023-03-28 11:17:43 +0200

    create basic list for the tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// todo infer currentTab type from allTabs
+const Tabs = ({allTabs, currentTab}: {allTabs: string[], currentTab: string}) => {
+  const [activeTab, setShowActive] = useState(currentTab)
+  const tabs = new Map<string, boolean>()
+  allTabs.map((tab) => tabs.set(tab, false))
+// ToDos
+// accept Multiple ReactNode as props
+// accept ReactNode instead of h2
+return (
+    <>
+    {allTabs.map((tab) => (
+      <button onClick={() => setShowActive(tab)}>{tab}</button>
+    ))}
+    <ul role='tablist'>
+    <li role='tabpanel' aria-hidden={!tabs.get(activeTab)}>
+      <h2>{activeTab}</h2>
+    </li>
+</ul>
+</>
+  )
+}
 
+function App() {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Tabs allTabs={['test1', 'test2', 'test3']} currentTab='test1'  />
     </div>
   )
 }
